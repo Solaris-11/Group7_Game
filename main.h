aecb2de68989c9cb21f0a984093aa9ec7fc5a10f
Shannon Chen <159525082+Shannon-whatever@users.noreply.github.com>
2024-04-22 21:32:52 +0800

    Update main.h

--- main.h
+++ main.h
@@ -18,36 +18,6 @@ struct Board{
   int numCols;  // The number of columns in the table
   int numF;   // The number of cards filpped at a time
 
-  // Function: setNonCanonicalMode
-  // Set the terminal to non-canonical mode
-  void setNonCanonicalMode() {
-    struct termios term;
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-  }
-
-  // Function: restoreTerminalMode
-  // Restore the terminal to canonical mode
-  void restoreTerminalMode() {
-    struct termios term;
-    tcgetattr(STDIN_FILENO, &term);
-    term.c_lflag |= ICANON | ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &term);
-  }
-
-  // Function: clearScreen
-  // Clear the terminal screen
-  void clearScreen() {
-    cout << "\033[2J";
-  }
-
-  // Funtion: moveCursorTo
-  // Move the terminal cursor to the specified position
-  void moveCursorTo(int x, int y) {
-    cout << "\033[" << y << ";" << x << "H";
-  }
-
   // Function: drawTable
   // Nested for loop to draw the table
   // Two loops to print the indicator for the selected row and column
